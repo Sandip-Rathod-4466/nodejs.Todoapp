3ebc72c5b146892e481aaf5914246a544653e817
Sandip-Rathod-4466 <120707076+Sandip-Rathod-4466@users.noreply.github.com>
2023-04-10 15:41:06 +0530

    update origin
    local

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = require("./app.js");
 app.use(cors({
-    origin:[process.env.FRONTEND_URI],
+    origin:[process.env.FRONTEND_URI,"http://localhost:3000"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }));
