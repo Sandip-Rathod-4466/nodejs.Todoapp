529f241de6c99b7ee9474cc709172f11fde83681
Sandip-Rathod-4466 <120707076+Sandip-Rathod-4466@users.noreply.github.com>
2023-04-12 09:58:02 +0530

    done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,29 +2,20 @@ const User = require("../models/user.js")
 const jwt = require("jsonwebtoken");
 
 const isAuthenticate = async (req,res,next)=>{
-    const { token } = req.cookies;
-    
-    if (!token) {
-        res.json({
-            status: false,
-            message: "Login first"
-        });
-    } 
-    else {
-        const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const { token } = await req.cookies;
 
-        const { _id } = decoded;
-        const user = await User.findById(_id);
-        if(user){
-            req.user = user;
-            next();
-        }else{
-            res.json({
-                status: false,
-                message: "invalid token"
-            });
-        }
+    if (!token) {
+      res.json({
+        status: false,
+        token,
+        message: "Login first",
+      });
     }
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    const { _id } = decoded;
+    const user = await User.findById(_id);
+    req.user = user;
+    next();
 }
 
 
